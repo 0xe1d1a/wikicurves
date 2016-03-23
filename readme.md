ae1b6b95f0c729bb29dfc760e298387087521c77
0xe1d1a <el.diamantakos@gmail.com>
2016-03-23 19:27:21 +0100

    added a test case

--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@
 
 Clusterization of similar attention curves by using hadoop and spark
 
+  #ref: https://spark.apache.org/docs/0.9.0/api/pyspark/index.html
+  #http://spark.apache.org/docs/latest/programming-guide.html
+  #interpolation problem?
+  #lower case projects-pages normalize?
+  #partition by project?
--- wikicurves.py
+++ wikicurves.py
@@ -1,9 +1,10 @@
 from pyspark import SparkContext
-from pyspark.mllib import util
+from pyspark.conf import SparkConf
+#from pyspark.mllib import util
 from time import time
 from datetime import datetime, timedelta
 import sys, argparse, logging
-from numpy import array
+#from numpy import array
 
 global args
 
@@ -13,8 +14,8 @@ def quiet_logs( sc ):
   logger.LogManager.getLogger("org"). setLevel( logger.Level.ERROR )
   logger.LogManager.getLogger("akka").setLevel( logger.Level.ERROR )
 
-
-def create_context( ):
+#main loop to do things for everyfile
+def process_data( ):
   sc = SparkContext("local", "wikicurves")
   if args.silent:
     quiet_logs(sc)
@@ -27,26 +28,111 @@ def create_context( ):
     fname = path + "pagecounts-2014" + cur.strftime("%m") + cur.strftime("%d") \
             + "-" + cur.strftime("%H") + "*.gz"
     logging.info("Working on " + fname)
-    raw_data = sc.textFile(fname)
-    sep = raw_data.map(lambda line: line.split(" "))
-    sep = sep.map(lambda vals: (vals[1], vals[2]))
-    sep = sep.filter(lambda val: val[0][0] != '%')
-    car = sep.cartesian(sep).filter(lambda pair: pair[0] != pair[1])
-    print(car.take(10))
+    #raw_data = sc.textFile(fname)
+    #sep = raw_data.map(lambda line: line.split(" "))
+    #sep = sep.map(lambda vals: (vals[1], vals[2]))
+    #sep = sep.filter(lambda val: val[0][0] != '%')
+    #car = sep.cartesian(sep).filter(lambda pair: pair[0] != pair[1])
+    #print(car.take(10))
     return #debug
-
     cur+= onehour
 
-  #basic RDD data manipulation here, Note unpacking gz takes long time!
-  #ref: https://spark.apache.org/docs/0.9.0/api/pyspark/index.html
-  #http://spark.apache.org/docs/latest/programming-guide.html
 
+def init_combiner(x):
+  l = list()
+  l.append(x)
+  return l
+
+def merge_vals(v,c):
+  try:
+    v.append(c)
+    return v
+  except Exception, e:
+    print (v,c)
+    raise e
+
+def merge_combiners(v,c):
+  try:
+    return v+c
+  except Exception, e:
+    print (v,c)
+    raise e
+
+
+def test_case():
+  conf = SparkConf()
+  #speeds up the decompression a bit
+  conf.set("io.compression.codecs", "io.sensesecure.hadoop.xz.XZCodec")
+  sc = SparkContext(conf=conf)
+
+  path = "/user/hannesm/lsde/wikistats/2014/2014-01/"
+  fname1 = path + "pagecounts-20140101-000000.gz" #~180mb
+  fname2 = path + "pagecounts-20140101-010000.gz" #~180mb
+  rdata1 = sc.textFile(fname1)
+  rdata2 = sc.textFile(fname2)
+
+  global args
+
+  use_lists = args.list # ~ 8 minutes
+  use_str = args.string # ~ 7 minutes
+  use_grp = args.group # ~ 6 minutes
+  use_join = args.join # ~ 20 minutes
+
+  print use_lists , use_str, use_grp, use_join
+
+  # filter possible empty lines
+  mp1 = rdata1.filter(lambda line: line != None)
+  # split to columns
+  mp1 = mp1.map(lambda line: line.split(" "))
+  # filter possible corrupted lines missing columns
+  mp1 = mp1.filter(lambda lst: len(lst) == 4)
+  # sanity check
+  mp1 = mp1.filter(lambda lst: not (None in lst))
+
+  if use_lists:
+    # create tuple: (key, val) with
+    # "project_name page_name" as key and count as val
+    mp1 = mp1.map(lambda lst: (lst[0]+ " " + lst[1], int(lst[2])))
+  else:
+    mp1 = mp1.map(lambda lst: (lst[0]+ " " + lst[1], (lst[2])))
+
+
+  # same exact code as above
+  mp2 = rdata2.filter(lambda line: line != None)
+  mp2 = mp2.map(lambda line: line.split(" "))
+  mp2 = mp2.filter(lambda lst: len(lst) == 4)
+  mp2 = mp2.filter(lambda lst: not (None in lst))
+  if use_lists:
+    mp2 = mp2.map(lambda lst: (lst[0]+ " " + lst[1], int(lst[2])))
+  else:
+    mp2 = mp2.map(lambda lst: (lst[0]+ " " + lst[1], (lst[2])))
+
+  grp = None
+  if use_join:
+    #create key pairs of ("key", (val1, val2)) also keeping track of interpolation
+    grp = mp1.fullOuterJoin(mp2, 4)
+  else:
+    # create a union of the two rdds
+    grp = mp1.union(mp2)
+    if use_lists:
+      #create key pairs of ("key", list[val1,val2])
+      grp = grp.combineByKey( init_combiner, merge_vals, merge_combiners, 4)
+    elif use_str:
+      #create key pairs of ("key", "val1 val2")
+      grp = grp.reduceByKey(lambda x,y: x+" "+y, 4)
+    elif use_grp:
+      #create key pairs of ("key", iteratable)
+      grp = grp.groupByKey(4)
+
+  out = grp.take(150)
+  for e in out:
+    print (e)
 
 
 def main( args, loglevel ):
   logging.basicConfig(format="%(levelname)s %(message)s", level=loglevel)
   logging.info("Starting the script")
-  create_context()
+  test_case()
 
 #std entry point
 if __name__ == '__main__':
@@ -59,14 +145,34 @@ if __name__ == '__main__':
     fromfile_prefix_chars = '@' )
 
   # Parameters specification
-  parser.add_argument(
-    "directory",
-    help = "the path of the directory which holds the wiki dumps",
-    metavar = "dir")
+  #parser.add_argument(
+  #  "directory",
+  #  help = "the path of the directory which holds the wiki dumps",
+  #  metavar = "dir")
   parser.add_argument(
     "-v",
     "--verbose",
     help="increase output verbosity",
+    action="store_true")
+  parser.add_argument(
+    "-l",
+    "--list",
+    help="union & use list",
+    action="store_true")
+  parser.add_argument(
+    "-t",
+    "--string",
+    help="union & use string concat",
+    action="store_true")
+  parser.add_argument(
+    "-g",
+    "--group",
+    help="union & use group by",
+    action="store_true")
+  parser.add_argument(
+    "-j",
+    "--join",
+    help="use full outer join",
     action="store_true")
   parser.add_argument(
     "-s",
