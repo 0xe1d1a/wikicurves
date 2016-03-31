86f132c7ee7eec9a8f48f3cc3cb34e041902f0cd
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2016-03-31 17:17:10 +0200

    basic custom Kmeans clustering TODO: Improve centroid calculation

--- cluster.py
+++ cluster.py
@@ -1,11 +1,64 @@
+from random import randint
+
 from pyspark import SparkContext, SparkConf
-from pyspark.mllib.clustering import KMeans, KMeansModel
-from pyspark.mllib.linalg import Vectors
+
+
+class KMeans(object):
+    def __init__(self, sc, rdd, k, max_iterations=100):
+        self.sc = sc
+        self.rdd = rdd
+        self.k = k
+        self.max_iterations = max_iterations
+
+    def cluster(self):
+        old_centroids = None
+        centroids = self._initialise_centroids(self.rdd)
+        iteration = 0
+        while not self._should_stop(old_centroids, centroids, iteration):
+            old_centroids = centroids
+            iteration += 1
+            self._add_labels(centroids)
+            centroids = self._update_centroids(centroids)
+            centroids = self.sc.broadcast(centroids)
+        return centroids
+
+    def _initialise_centroids(self):
+        return self.rdd.take(self.k)
+
+    def _should_stop(self, old_centroids, centroids, iteration):
+        convergence = (old_centroids == centroids)
+        duration = (iteration >= self.max_iterations)
+        return convergence or duration
+
+    def _add_labels(self, centroids):
+        def min_dist(x):
+            min_dist = 10000
+            centroid = 0
+            for c in range(self.k):
+                dst = self.distance(x, centroids[c])
+                if dst < min_dist:
+                    min_dist = dst
+                    centroid = c
+            x.insert(0, centroid)  # assign centroid to instance
+
+        self.rdd.foreach(min_dist)
+
+    def _update_centroids(self):
+        def remove_label(x):
+            x.pop(0)
+
+        self.rdd.foreach(remove_label)
+        return self.rdd.take(self.k)
+
+    def distance(self, v1, v2):
+        return randint(1, 10000)
+
 
 def quiet_logs(sc):
     log = sc._jvm.org.apache.log4j
     log.LogManager.getLogger('org').setLevel(log.Level.ERROR)
     log.LogManager.getLogger('akka').setLevel(log.Level.ERROR)
+
 
 app_name = 'wikicurves'
 conf = SparkConf().setAppName(app_name)
@@ -14,12 +67,11 @@ sc = SparkContext(conf=conf)
 quiet_logs(sc)
 log = sc._jvm.org.apache.log4j.Logger.getLogger(__name__)
 
-filename = '/user/lsde10/fin/part-00000'
+FILENAME = '/user/lsde10/fin3/*'
 
-data = sc.pickleFile(filename)
-parsed_data = data.map(lambda x: Vectors.dense(x[1]))
+data = sc.textFile(FILENAME)
+parsed_data = data.map(lambda x: x.split(',')[2:])
 
-log.info('Starting k-means')
-clusters = KMeans.train(parsed_data, 3)
+centroids = KMeans(sc, parsed_data, 3)
 
-clusters.save(sc, '/user/lsde10/part-00000.model')
+log.info(centroids)
